ab2a292c21be7a67d99569f34916cfe0d4d57eea
nabaff01 <nabaff01@gmail.com>
2025-09-24 22:34:02 +0000

    testing commit

--- main.py
+++ main.py
@@ -13,14 +13,14 @@
 """
 
 
-def get_user_name():
+def ge_user_name():
     return input("Please enter your name: ")
 
 def greet_user(name):
     print(f"Hello, {name}, how are you?")
 
 def main():
-    user_name = get_user_name()
+    user_name = ge_user_name()
     greet_user(user_name)
 
 if __name__ == "__main__":
